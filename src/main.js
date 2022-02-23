2c59656a0f73d75e6865db729e1eb18467c5ed27
quentin guirois <quentin.guirois@gmail.com>
2022-02-23 12:31:17 +0100

    initialisation i18n

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "ja",
+ globalInjection: true,
+  messages: {
+    en: {
+      message: {
+        language: "English",
+        greeting: "Hello !"
+      }
+    },
+    ar: {
+      message: {
+        language: "العربية",
+        greeting: "السلام عليكم"
+      }
+    },
+    es: {
+      message: {
+        language: "Español",
+        greeting: "Hola !"
+      }
+    }
+  }
+});
+
+createApp(App).use(i18n).mount("#app");
+
+
+
+
+
+
